df0b3768ff0d4489c63b4082c924edb6377a3c13
Archaeologist03 <omniiknight@hotmail.com>
2019-01-31 22:50:24 +0100

    Updating readme with todo. Small tweaks on drumPad.

--- src/components/DrumPad/DrumPad.js
+++ src/components/DrumPad/DrumPad.js
@@ -1,15 +1,40 @@
-import React from 'react'
+import React from 'react';
 
-function DrumPad(props) {
-  return (
-    <div className="drumPad-container">
-      <div>
-        <button onClick={() => props.playSound(props.sound)}>
-          {props.keyName}
-        </button>
+class DrumPad extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicked: false,
+      count: 0,
+      shouldPlay: false,
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if ((prevState.clicked !== this.state.clicked) && prevState.count + 1 === this.state.count ) {
+    //   this.setState({
+    //     shouldPlay: true,
+    //   })
+    // }
+    // else {
+    //   this.setState({
+    //     shouldPlay: false,
+    //   })
+    console.log(prevState.clicked, this.state.clicked);
+
+    }
+    
+  }
+
+  render(props) {
+    return (
+      <div className="drumPad-container" onClick={() => this.setState({clicked: !this.state.clicked, count: this.state.count + 1})}>
+        {this.state.shouldPlay ? <audio src={this.props.sound} autoPlay/> : null}
+        {this.props.keyName}
       </div>
-    </div>
-  )
+    );
+  }
 }
 
-export default DrumPad
+export default DrumPad;
+// onClick={() => props.playSound(props.sound)}
